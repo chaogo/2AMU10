8eadb8b242edf445462c0f6529c0082995269281
wouterhoffman <w.f.c.hoffman@student.tue.nl>
2021-11-26 15:34:50 +0100

    work in progress 2

--- Team6_A0/sudokuai.py
+++ Team6_A0/sudokuai.py
@@ -22,12 +22,72 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
         n = game_state.board.n
         m = game_state.board.m
 
-        def possible(i, j, value):
-            return game_state.board.get(i, j) == SudokuBoard.empty and not TabooMove(i, j,
-                                                                                     value) in game_state.taboo_moves
+        def calculate_move_score(self, N, n, m, board, i, j, value, maximizing):
+            """"
+            The evaluation function evaluates the score in the game that will be obtained by the proposed move (i,j,value)
+            """
+            # Add our hypothetical move to the current situation on the board
+            board.put(i, j, value)
 
-        def legal(possible_moves):
+            # Create variables for each region that could be completed by a move (row, column, region)
+            # Where 1 = filled and 0 = not filled. All initialized to 1.
+            row = 1
+            column = 1
+            block = 1
 
+            # CHECK ROW: loop through all k values in row i
+            for k in range(N):
+                # Check if we find an empty cell in row i column k
+                if board.get(i, k) == SudokuBoard.empty:
+                    row = 0
+                    break
+
+            # CHECK COLUMN: loop through all l values in column j
+            for l in range(N):
+                # Check if we find an empty cell in column j row l
+                if board.get(l, j) == SudokuBoard.empty:
+                    column = 0
+                    break
+
+            """
+            ADD HERE:         # CHECK block: loop through all n*m
+            """
+
+            # the total score in the game earned with this move
+            score = 0
+            regions_filled = row + column + block
+            if maximizing:
+                # Calculate the total score in the game for current move and return the score
+                if regions_filled == 0:
+                    score = 0
+                elif regions_filled == 1:
+                    score = 1
+                elif regions_filled == 2:
+                    score = 3
+                elif regions_filled == 3:
+                    score = 7
+
+            if not maximizing:
+                # Calculate the negated total score in the game for current move of the opposition
+                if regions_filled == 0:
+                    score = 0
+                elif regions_filled == 1:
+                    score = -1
+                elif regions_filled == 2:
+                    score = -3
+                elif regions_filled == 3:
+                    score = -7
+
+            return score
+
+        """ Returns all legal moves given a board """
+
+        def get_all_legal_moves(board):
+
+            # Generate all moves that are possible
+            possible_moves = [Move(i, j, value) for i in range(N) for j in range(N) for value in range(1, N + 1) if
+                              board.get(i, j) == SudokuBoard.empty and not TabooMove(i, j,
+                                                                                     value) in game_state.taboo_moves]
             # filter out illegal moves
             all_moves_legal = []
 
@@ -46,90 +106,25 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
                 if legal:
                     # Check if value already in column
                     for l in range(N):
-                        if game_state.board.get(l, j) == value:
+                        if board.get(l, j) == value:
                             legal = False
                             break
 
-                if legal:
+                """if legal:
                     # Check if value already in block
-                    block_position_verti = i // m
-                    block_position_hori = j // n
+                    block_position_verti = i // m * m
+                    block_position_hori = j // n * n
 
                     for k in range(, )
                     for l in range()
+                    """
 
-            # If all checks are passed add the move to the list of legal moves
-            if legal:
-                all_moves_legal.append(move)
+                # If all checks are passed add the move to the list of legal moves
+                if legal:
+                    all_moves_legal.append(move)
 
-    # Generate all moves that are possible
-    all_moves = [Move(i, j, value) for i in range(N) for j in range(N) for value in range(1, N + 1) if
-                 possible(i, j, value)]
-
-    # Filter out moves that are illegal (do not comply with the game rules)
-    legal_moves = legal(all_moves)
+            return all_moves_legal
 
     # Propose naive move
     move = random.choice(all_moves)
     self.propose_move(move)
-
-
-def eval(self, N, n, m, game_state: GameState, i, j, value):
-    """"
-    The eval function evaluates the score in the game that will be obtained by the proposed move (i,j,value)
-    """
-    # Add our hypothetical move to the current situation on the board
-    game_state.board.put(i, j, value)
-
-    # Create variables for each region that could be completed by a move (row, column, region)
-    # Where 1 = filled and 0 = not filled. All initialized to 1.
-    row = 1
-    column = 1
-    block = 1
-
-    # CHECK ROW: loop through all k values in row i
-    for k in range(N):
-        # Check if we find an empty cell in row i column k
-        if game_state.board.get(i, k) == SudokuBoard.empty:
-            row = 0
-            break
-
-    # CHECK COLUMN: loop through all l values in column j
-    for l in range(N):
-        # Check if we find an empty cell in column j row l
-        if game_state.board.get(l, j) == SudokuBoard.empty:
-            column = 0
-            break
-
-    # CHECK block: loop through all n*m
-
-    # the total score in the game earned with this move
-    score = 0
-    regions_filled = row + column + block
-
-    # Calculate the total score in the game for current move and return the score
-    if regions_filled == 0:
-        score = 0
-    elif regions_filled == 1:
-        score = 1
-    elif regions_filled == 2:
-        score = 3
-    elif regions_filled == 3:
-        score = 7
-
-    return score
-
-
-def minimax(self, position, depth, maximizing):
-    move = ''
-
-    if depth == 0:
-        return move
-
-    if maximizing:
-        print('maxi')
-    else:
-        print('mini')
-
-    return move
-
